2291644593ba98e4de45540b8477c69d23ff725e
CHARLOTTE Frates <charlottefrates@gmail.com>
2017-03-30 13:01:50 -0700

    commit 14: added About modal window

--- app.js
+++ app.js
@@ -149,6 +149,9 @@ $('.required').keydown(function(){
 
 
 
+
+
+
 $(document).ready(function () {
      $('#enter').on('click',function(){
           $('.container').removeClass('hidden');
@@ -156,4 +159,34 @@ $(document).ready(function () {
           $('.html').addClass('background');
           $('#sidebar').removeClass('hidden');
      });
+
+     // Get the modal
+     var modal = document.getElementById('myModal');
+
+     // Get the button that opens the modal
+     var btn = document.getElementById("myBtn");
+
+     // Get the <span> element that closes the modal
+     var span = document.getElementsByClassName("close")[0];
+
+     // When the user clicks the button, open the modal
+     $('#about').on('click', function() {
+         modal.style.display = "block";
+    });
+
+     // When the user clicks on <span> (x), close the modal
+     span.onclick = function() {
+         modal.style.display = "none";
+    };
+
+     // When the user clicks anywhere outside of the modal, close it
+     window.onclick = function(event) {
+         if (event.target == modal) {
+             modal.style.display = "none";
+         }
+    };
+
+
+
+
 });
